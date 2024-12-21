e85c68b35ba633a111e9de1b6ef2e3cdffcf16d0
exyoyou <64944848+exyoyou@users.noreply.github.com>
2024-12-21 20:59:25 +0800

    Update config.rs
    地址错误

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["http://rustdesk.home-yyou.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.home-yyou.top"];
 pub const PUBLIC_RS_PUB_KEY: &str = "GNK+EcDuZaxzps0TUunXrTb8v2bZIirHyp8ZEMyYNuM=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
